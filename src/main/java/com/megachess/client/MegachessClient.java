5812937e81b4ef0e49b3fd3334cb461074d9597e
Emile Caron <emicaron95@gmail.com>
2020-11-17 21:30:37 +0000

    Added basic message interpretation with server

--- src/main/java/com/megachess/client/MegachessClient.java
+++ src/main/java/com/megachess/client/MegachessClient.java
@@ -41,7 +41,67 @@ public class MegachessClient extends WebSocketClient{
 
 	@Override
 	public void onMessage(String message) {
-            System.out.println(message);
+            
+            JSONObject receivedMessage = new JSONObject(message); //Mensaje Recibido.
+            JSONObject messageToSend = new JSONObject();    //Respuesta.
+            JSONObject data = new JSONObject(); //Objeto de data en la respuesta.
+            
+            String event = receivedMessage.getString("event"); 
+            
+            switch (event){
+                case "update_user_list":
+
+                    System.out.println(receivedMessage.toString());
+                    messageToSend.put("action", "challenge");
+                    data.put("username", "Emile");
+                    data.put("message", "Te reto a un duelo.");
+                    messageToSend.put("data", data);
+                    System.out.println(messageToSend.toString());
+                    try{
+                        send(messageToSend.toString());
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                    
+                case "ask_challenge":
+                    
+                    System.out.println(receivedMessage.toString());
+                    messageToSend.put("action", "accept_challenge");
+                    data.put("board_id", receivedMessage.getJSONObject("data").get("board_id"));
+                    messageToSend.put("data", data);
+                    System.out.println(messageToSend.toString());
+                    try{
+                        send(messageToSend.toString());
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                    
+                case "your_turn":
+                    
+                    System.out.println(receivedMessage.toString());
+                    messageToSend.put("action", "move");
+                    data.put("board_id", receivedMessage.getJSONObject("data").get("board_id"));
+                    data.put("turn_token", receivedMessage.getJSONObject("data").get("turn_token"));
+                    data.put("from_row", Math.floor(Math.random()*16));
+                    data.put("from_col", Math.floor(Math.random()*16));
+                    data.put("to_row", Math.floor(Math.random()*16));
+                    data.put("to_col", Math.floor(Math.random()*16));
+                    messageToSend.put("data", data);
+                    System.out.println(messageToSend.toString());
+                    try{
+                        send(messageToSend.toString());
+                    }catch(Exception e){
+                        System.out.println(e.toString());
+                    }
+                    break;
+                    
+                case "gameover":
+                    
+                    System.out.println(receivedMessage.toString());
+                    break;
+            }
         }
         
     	@Override
