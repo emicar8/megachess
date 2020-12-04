90c4602ef72e1339d68232e340163e8c5423b64d
emile caron <emicaron95@gmail.com>
2020-12-04 17:04:18 -0300

    Added bishop tests and cleaned up bishop logic

--- src/main/java/com/megachess/chesspiece/Bishop.java
+++ src/main/java/com/megachess/chesspiece/Bishop.java
@@ -19,16 +19,12 @@ public class Bishop extends ChessPiece{
         this.pointsForKill = 400;
     }    
 
-     private void moveRightAndDown(int newRow, int newCol, List<List<ChessPiece>> Board){
+     public void moveRightAndDown(int newRow, int newCol, List<List<ChessPiece>> Board){
         if(newRow < 16 && newCol < 16){
             if(!Board.get(newRow).get(newCol).isNull()){ //Position is not empty
-                if(Board.get(newRow).get(newCol).getColor().equals(this.color)){ //Check if piece is same color
-                    if(this.currentRow != newRow - 1 && this.currentCol != newCol - 1){ //Avoid adding current position as possible move
-                        this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow - 1, newCol - 1, this.getPointsForMove()}); //Piece is same color
-                    }                    
-                }else{
-                    this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow, newCol, Board.get(newRow).get(newCol).getPointsForKill()}); //Piece is not same color
-                }                
+                if(!Board.get(newRow).get(newCol).getColor().equals(this.color)){ //Check if piece is different color
+                    this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow, newCol, Board.get(newRow).get(newCol).getPointsForKill()}); //Piece is not same color                  
+                }                 
             }else{ //Can keep moving.
                 this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow, newCol, this.getPointsForMove()});
                 this.moveRightAndDown(newRow + 1, newCol + 1, Board);
@@ -36,16 +32,12 @@ public class Bishop extends ChessPiece{
         }
     }    
     
-      private void moveRightAndUp(int newRow, int newCol, List<List<ChessPiece>> Board){
+      public void moveRightAndUp(int newRow, int newCol, List<List<ChessPiece>> Board){
         if(newRow > -1 && newCol < 16){
             if(!Board.get(newRow).get(newCol).isNull()){ //Position is not empty
-                if(Board.get(newRow).get(newCol).getColor().equals(this.color)){ //Check if piece is same color
-                    if(this.currentRow != newRow + 1 && this.currentCol != newCol - 1){ //Avoid adding current position as possible move
-                        this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow + 1, newCol - 1, this.getPointsForMove()}); //Piece is same color
-                    }                    
-                }else{
-                    this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow, newCol, Board.get(newRow).get(newCol).getPointsForKill()}); //Piece is not same color
-                }                
+                if(!Board.get(newRow).get(newCol).getColor().equals(this.color)){ //Check if piece is different color
+                    this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow, newCol, Board.get(newRow).get(newCol).getPointsForKill()}); //Piece is not same color                  
+                }               
             }else{ //Can keep moving.
                 this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow, newCol, this.getPointsForMove()});
                 this.moveRightAndUp(newRow - 1, newCol + 1, Board);
@@ -53,16 +45,12 @@ public class Bishop extends ChessPiece{
         }
     }  
       
-     private void moveLeftAndUp(int newRow, int newCol, List<List<ChessPiece>> Board){
+     public void moveLeftAndUp(int newRow, int newCol, List<List<ChessPiece>> Board){
         if(newRow > -1 && newCol > -1){
             if(!Board.get(newRow).get(newCol).isNull()){ //Position is not empty
-                if(Board.get(newRow).get(newCol).getColor().equals(this.color)){ //Check if piece is same color
-                    if(this.currentRow != newRow + 1 && this.currentCol != newCol + 1){ //Avoid adding current position as possible move
-                        this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow + 1, newCol + 1, this.getPointsForMove()}); //Piece is same color
-                    }                    
-                }else{
-                    this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow, newCol, Board.get(newRow).get(newCol).getPointsForKill()}); //Piece is not same color
-                }                
+                if(!Board.get(newRow).get(newCol).getColor().equals(this.color)){ //Check if piece is different color
+                    this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow, newCol, Board.get(newRow).get(newCol).getPointsForKill()}); //Piece is not same color                  
+                }                 
             }else{ //Can keep moving.
                 this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow, newCol, this.getPointsForMove()});
                 this.moveLeftAndUp(newRow - 1, newCol - 1, Board);
@@ -70,16 +58,12 @@ public class Bishop extends ChessPiece{
         }
     }        
 
-     private void moveLeftAndDown(int newRow, int newCol, List<List<ChessPiece>> Board){
+     public void moveLeftAndDown(int newRow, int newCol, List<List<ChessPiece>> Board){
         if(newRow < 16 && newCol > -1){
             if(!Board.get(newRow).get(newCol).isNull()){ //Position is not empty
-                if(Board.get(newRow).get(newCol).getColor().equals(this.color)){ //Check if piece is same color
-                    if(this.currentRow != newRow - 1 && this.currentCol != newCol + 1){ //Avoid adding current position as possible move
-                        this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow - 1, newCol + 1, this.getPointsForMove()}); //Piece is same color
-                    }                    
-                }else{
-                    this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow, newCol, Board.get(newRow).get(newCol).getPointsForKill()}); //Piece is not same color
-                }                
+                if(!Board.get(newRow).get(newCol).getColor().equals(this.color)){ //Check if piece is different color
+                    this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow, newCol, Board.get(newRow).get(newCol).getPointsForKill()}); //Piece is not same color                  
+                }                 
             }else{ //Can keep moving.
                 this.possibleMoves.add(new int[]{this.currentRow, this.currentCol, newRow, newCol, this.getPointsForMove()});
                 this.moveLeftAndDown(newRow + 1, newCol - 1, Board);
---
+++ src/test/java/com/megachess/chesspiece/BishopTest.java
@@ -0,0 +1,269 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.megachess.chesspiece;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ *
+ * @author Emile
+ */
+public class BishopTest {
+    
+    public BishopTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    private List<List<ChessPiece>> generateBoard(String boardString){
+        List<List<ChessPiece>> Board = new ArrayList<>();
+        for(int row = 0; row < 16; row++){
+            List<ChessPiece> BoardRow = new ArrayList<>();
+            for(int column = 0; column < 16; column++){
+                switch(boardString.charAt(row*16 + column)){
+                    case 'p':
+                       BoardRow.add(new Pawn(row,column,"black"));
+                       break;
+                    case 'P':
+                        BoardRow.add(new Pawn(row,column,"white"));
+                        break;                               
+                   case 'r':
+                       BoardRow.add(new Rook(row,column,"black")); 
+                       break;
+                    case 'R':
+                        BoardRow.add(new Rook(row,column,"white")); 
+                        break;                               
+                   case 'h':
+                       BoardRow.add(new Knight(row,column,"black"));
+                       break;
+                    case 'H':
+                        BoardRow.add(new Knight(row,column,"white")); 
+                        break;                               
+                   case 'b':
+                       BoardRow.add(new Bishop(row,column,"black")); 
+                       break;
+                    case 'B':
+                        BoardRow.add(new Bishop(row,column,"white"));
+                        break;                               
+                   case 'q':
+                       BoardRow.add(new Queen(row,column,"black"));
+                       break;
+                    case 'Q':
+                        BoardRow.add(new Queen(row,column,"white"));
+                        break;                               
+                   case 'k':
+                       BoardRow.add(new King(row,column,"black"));
+                       break;                          
+                    case 'K':
+                        BoardRow.add(new King(row,column,"white")); 
+                        break;
+                    default:
+                        BoardRow.add(new NullPiece(row,column));
+                        break;                        
+                }                        
+            }
+            Board.add(BoardRow);
+        } 
+        return Board;
+    }
+
+    //////////////////////////////MOVE RIGHT AND UP TEST/////////////////////////////////////////////////////////////////////////////////
+    
+    static Stream<Arguments> testMoveRightAndUp_Parameters(){
+        Pawn AuxPawn = new Pawn(0,0,"");
+        Bishop AuxBishop = new Bishop(0,0,"");
+        List<int[]> TestResult1 = new ArrayList<>(); //Adjacent piece of same color, no moves should be generated
+        List<int[]> TestResult2 = new ArrayList<>(); //Piece of different color, with possible movement and attack.
+        TestResult2.add(new int[] {3,12,2,13,AuxBishop.getPointsForMove()});
+        TestResult2.add(new int[] {3,12,1,14,AuxPawn.getPointsForKill()});
+        List<int[]> TestResult3 = new ArrayList<>(); //Piece of same color but with possible movement
+        TestResult3.add(new int[] {3,12,2,13,AuxBishop.getPointsForMove()});
+        List<int[]> TestResult4 = new ArrayList<>(); //No Pieces in the way, moves till the border
+        TestResult4.add(new int[] {3,12,2,13,AuxBishop.getPointsForMove()});
+        TestResult4.add(new int[] {3,12,1,14,AuxBishop.getPointsForMove()});
+        TestResult4.add(new int[] {3,12,0,15,AuxBishop.getPointsForMove()});
+        List<int[]> TestResult5 = new ArrayList<>(); //Piece on the border, no move possible
+        
+        return Stream.of(
+                Arguments.of("                                             p              b                                                                                                                                                                                                   ", 3, 12, TestResult1),
+                Arguments.of("                              P                             b                                                                                                                                                                                                   ", 3, 12, TestResult2),
+                Arguments.of("                              p                             b                                                                                                                                                                                                   ", 3, 12, TestResult3),
+                Arguments.of("                                                            b                                                                                                                                                                                                   ", 3, 12, TestResult4),
+                Arguments.of("               b                                                                                                                                                                                                                                                ", 0, 15, TestResult5)              
+        );
+    }    
+    
+    
+    @ParameterizedTest(name="Bishop move right and up test run {index}")
+    @MethodSource("testMoveRightAndUp_Parameters")
+    public void testMoveRightAndUp(String boardString, int currentRow, int currentCol, List<int[]> expectedMoves){
+        //Create board
+        List<List<ChessPiece>> Board = this.generateBoard(boardString);
+        //Finished creating board
+        
+        Bishop TestBishop = new Bishop(currentRow,currentCol,"black");
+        TestBishop.moveRightAndUp(currentRow - 1, currentCol + 1, Board);
+        assertArrayEquals(expectedMoves.toArray(), TestBishop.getPossibleMoves().toArray());
+        
+    }
+    
+    //////////////////////////////MOVE RIGHT AND DOWN TEST/////////////////////////////////////////////////////////////////////////////////
+    
+    static Stream<Arguments> testMoveRightAndDown_Parameters(){
+        Pawn AuxPawn = new Pawn(0,0,"");
+        Bishop AuxBishop = new Bishop(0,0,"");
+        List<int[]> TestResult1 = new ArrayList<>(); //Adjacent piece of same color, no moves should be generated
+        List<int[]> TestResult2 = new ArrayList<>(); //Piece of different color, with possible movement and attack.
+        TestResult2.add(new int[] {12,12,13,13,AuxBishop.getPointsForMove()});
+        TestResult2.add(new int[] {12,12,14,14,AuxPawn.getPointsForKill()});
+        List<int[]> TestResult3 = new ArrayList<>(); //Piece of same color but with possible movement
+        TestResult3.add(new int[] {12,12,13,13,AuxBishop.getPointsForMove()});
+        List<int[]> TestResult4 = new ArrayList<>(); //No Pieces in the way, moves till the border
+        TestResult4.add(new int[] {12,12,13,13,AuxBishop.getPointsForMove()});
+        TestResult4.add(new int[] {12,12,14,14,AuxBishop.getPointsForMove()});
+        TestResult4.add(new int[] {12,12,15,15,AuxBishop.getPointsForMove()});
+        List<int[]> TestResult5 = new ArrayList<>(); //Piece on the border, no move possible
+        
+        return Stream.of(
+                Arguments.of("                                                                                                                                                                                                            b                p                                  ", 12, 12, TestResult1),
+                Arguments.of("                                                                                                                                                                                                            b                                 P                 ", 12, 12, TestResult2),
+                Arguments.of("                                                                                                                                                                                                            b                                 p                 ", 12, 12, TestResult3),
+                Arguments.of("                                                                                                                                                                                                            b                                                   ", 12, 12, TestResult4),
+                Arguments.of("                                                                                                                                                                                                                                                               b", 15, 15, TestResult5)              
+        );
+    }    
+    
+    
+    @ParameterizedTest(name="Bishop move right and down test run {index}")
+    @MethodSource("testMoveRightAndDown_Parameters")
+    public void testMoveRightAndDown(String boardString, int currentRow, int currentCol, List<int[]> expectedMoves){
+        //Create board
+        List<List<ChessPiece>> Board = this.generateBoard(boardString);
+        //Finished creating board
+        
+        Bishop TestBishop = new Bishop(currentRow,currentCol,"black");
+        TestBishop.moveRightAndDown(currentRow + 1, currentCol + 1, Board);
+        assertArrayEquals(expectedMoves.toArray(), TestBishop.getPossibleMoves().toArray());
+        
+    }    
+    
+    //////////////////////////////MOVE LEFT AND UP TEST/////////////////////////////////////////////////////////////////////////////////
+    
+    static Stream<Arguments> testMoveLeftAndUp_Parameters(){
+        Pawn AuxPawn = new Pawn(0,0,"");
+        Bishop AuxBishop = new Bishop(0,0,"");
+        List<int[]> TestResult1 = new ArrayList<>(); //Adjacent piece of same color, no moves should be generated
+        List<int[]> TestResult2 = new ArrayList<>(); //Piece of different color, with possible movement and attack.
+        TestResult2.add(new int[] {3,3,2,2,AuxBishop.getPointsForMove()});
+        TestResult2.add(new int[] {3,3,1,1,AuxPawn.getPointsForKill()});
+        List<int[]> TestResult3 = new ArrayList<>(); //Piece of same color but with possible movement
+        TestResult3.add(new int[] {3,3,2,2,AuxBishop.getPointsForMove()});
+        List<int[]> TestResult4 = new ArrayList<>(); //No Pieces in the way, moves till the border
+        TestResult4.add(new int[] {3,3,2,2,AuxBishop.getPointsForMove()});
+        TestResult4.add(new int[] {3,3,1,1,AuxBishop.getPointsForMove()});
+        TestResult4.add(new int[] {3,3,0,0,AuxBishop.getPointsForMove()});
+        List<int[]> TestResult5 = new ArrayList<>(); //Piece on the border, no move possible
+        
+        return Stream.of(
+                Arguments.of("                                  p                b                                                                                                                                                                                                            ", 3, 3, TestResult1),
+                Arguments.of("                 P                                 b                                                                                                                                                                                                            ", 3, 3, TestResult2),
+                Arguments.of("                 p                                 b                                                                                                                                                                                                            ", 3, 3, TestResult3),
+                Arguments.of("                                                   b                                                                                                                                                                                                            ", 3, 3, TestResult4),
+                Arguments.of("b                                                                                                                                                                                                                                                               ", 0, 0, TestResult5)              
+        );
+    }    
+    
+    
+    @ParameterizedTest(name="Bishop move left and up test run {index}")
+    @MethodSource("testMoveLeftAndUp_Parameters")
+    public void testMoveLeftAndUp(String boardString, int currentRow, int currentCol, List<int[]> expectedMoves){
+        //Create board
+        List<List<ChessPiece>> Board = this.generateBoard(boardString);
+        //Finished creating board
+        
+        Bishop TestBishop = new Bishop(currentRow,currentCol,"black");
+        TestBishop.moveLeftAndUp(currentRow - 1, currentCol - 1, Board);
+        assertArrayEquals(expectedMoves.toArray(), TestBishop.getPossibleMoves().toArray());
+        
+    }  
+
+    //////////////////////////////MOVE LEFT AND DOWN TEST/////////////////////////////////////////////////////////////////////////////////
+    
+    static Stream<Arguments> testMoveLeftAndDown_Parameters(){
+        Pawn AuxPawn = new Pawn(0,0,"");
+        Bishop AuxBishop = new Bishop(0,0,"");
+        List<int[]> TestResult1 = new ArrayList<>(); //Adjacent piece of same color, no moves should be generated
+        List<int[]> TestResult2 = new ArrayList<>(); //Piece of different color, with possible movement and attack.
+        TestResult2.add(new int[] {12,3,13,2,AuxBishop.getPointsForMove()});
+        TestResult2.add(new int[] {12,3,14,1,AuxPawn.getPointsForKill()});
+        List<int[]> TestResult3 = new ArrayList<>(); //Piece of same color but with possible movement
+        TestResult3.add(new int[] {12,3,13,2,AuxBishop.getPointsForMove()});
+        List<int[]> TestResult4 = new ArrayList<>(); //No Pieces in the way, moves till the border
+        TestResult4.add(new int[] {12,3,13,2,AuxBishop.getPointsForMove()});
+        TestResult4.add(new int[] {12,3,14,1,AuxBishop.getPointsForMove()});
+        TestResult4.add(new int[] {12,3,15,0,AuxBishop.getPointsForMove()});
+        List<int[]> TestResult5 = new ArrayList<>(); //Piece on the border, no move possible
+        
+        return Stream.of(
+                Arguments.of("                                                                                                                                                                                                   b              p                                             ", 12, 3, TestResult1),
+                Arguments.of("                                                                                                                                                                                                   b                             P                              ", 12, 3, TestResult2),
+                Arguments.of("                                                                                                                                                                                                   b                             p                              ", 12, 3, TestResult3),
+                Arguments.of("                                                                                                                                                                                                   b                                                            ", 12, 3, TestResult4),
+                Arguments.of("                                                                                                                                                                                                                                                b               ", 15, 0, TestResult5)              
+        );
+    }    
+    
+    
+    @ParameterizedTest(name="Bishop move left and down test run {index}")
+    @MethodSource("testMoveLeftAndDown_Parameters")
+    public void testMoveLeftAndDown(String boardString, int currentRow, int currentCol, List<int[]> expectedMoves){
+        //Create board
+        List<List<ChessPiece>> Board = this.generateBoard(boardString);
+        //Finished creating board
+        
+        Bishop TestBishop = new Bishop(currentRow,currentCol,"black");
+        TestBishop.moveLeftAndDown(currentRow + 1, currentCol - 1, Board);
+        assertArrayEquals(expectedMoves.toArray(), TestBishop.getPossibleMoves().toArray());
+        
+    }    
+    
+    /**
+     * Test of isNull method, of class Bishop.
+     */
+    @Test
+    public void testIsNull() {
+        Bishop instance = new Bishop(0,0,"");
+        boolean expResult = false;
+        boolean result = instance.isNull();
+        assertEquals(expResult, result);
+    }
+    
+}
